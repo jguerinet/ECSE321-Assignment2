f4e7fa38dcefcd59280bddcad8cbd1796d387ba9
Julien Guerinet <julien@guerinet.com>
2013-10-03 21:57:31 -0400

    Commented the code

--- PrintPrimes.java
+++ PrintPrimes.java
@@ -1,10 +1,16 @@
 public class PrintPrimes {
+	//Number of Primes we are looking for
 	int numberOfPrimes;
+	//Number of rows per page 
 	int numberOfRows;
+	//Number of columns per page
 	int numberOfColumns;
+	//Array size where we store the non-prime numbers to compare with each number
 	int nonPrimeNumbersArraySize;
+	//The Array containing the final list of primes. 
 	int listOfPrimes[];
 
+	//Constructor method
 	public PrintPrimes(int numberOfPrimes, int numberOfRows, int numberOfColumns, int nonPrimeNumbersArraySize) {
 		this.numberOfPrimes   = numberOfPrimes;
 		this.numberOfRows  = numberOfRows;
@@ -34,14 +40,22 @@ public class PrintPrimes {
 		boolean isPrime;
 		int nonPrimeNumbers[] = new int[nonPrimeNumbersArraySize + 1];
 
+		//Number to analyze
 		int currentNumber = 1;
 		//This will keep the index of the prime number that is the square root of the next square we will encounter.
 		int primeNumberIndex = 2;
+		//The square of the next prime number. 
 		int square = 9;
 
 		for (int primesFoundSoFar = 2; primesFoundSoFar <= numberOfPrimes; primesFoundSoFar++) {
 			do {
+				//Increment by 2 because we are only looking for odd numbers
 				currentNumber = currentNumber + 2;
+				/*If the current number is the stored square: 
+				 * 	- Increment primeNumberIndex
+				 * 	- Calculate the next square
+				 * 	- Store the current number in the nonPrimeNumbers arraylist
+				*/
 				if (currentNumber == square) {
 					primeNumberIndex = primeNumberIndex + 1;
 					square = listOfPrimes[primeNumberIndex] * listOfPrimes[primeNumberIndex];
@@ -53,12 +67,15 @@ public class PrintPrimes {
 					while (nonPrimeNumbers[i] < currentNumber){
 						nonPrimeNumbers[i] = nonPrimeNumbers[i] + listOfPrimes[i] + listOfPrimes[i];
 					}
+					//If the current number equals to a number in the nonPrimeNumbers Arraylist, it cannot be prime.
 					if (nonPrimeNumbers[i] == currentNumber){
 						isPrime = false;
 					}
+					//Increment i
 					i = i + 1;
 				}
 			} while (!isPrime);
+			//Store the current number (that is prime) at the primesFoundSoFar index of listOfPrimes
 			listOfPrimes[primesFoundSoFar] = currentNumber;
 		}
 	}
@@ -67,18 +84,26 @@ public class PrintPrimes {
         int pageNumber = 1;
         int pageOffset = 1;
         while (pageOffset <= numberOfPrimes) {
+        	//Print this at the top of each page
         	System.out.println("The First " + numberOfPrimes +
                                " Prime Numbers --- Page " + pageNumber);
+        	//Blank line to separate header from numbers
         	System.out.println("");
         	for (int rowOffset = pageOffset; rowOffset < pageOffset + numberOfRows; rowOffset++){
+        		//Print column by column
         		for (int currentColumn = 0; currentColumn < numberOfColumns; currentColumn++)
+        			//Check that there are still numbers to print
         			if (rowOffset + currentColumn * numberOfRows <= numberOfPrimes){
         				System.out.format("%10d", listOfPrimes[rowOffset + currentColumn * numberOfRows]);
         			}
+        		//Go to the next line
         		System.out.println("");
         	}
+        	//Go to next 'page'
         	System.out.println("\f");
+        	//Increment page number
         	pageNumber = pageNumber + 1;
+        	//Increment page offset
         	pageOffset = pageOffset + numberOfRows * numberOfColumns;
         }
     }
